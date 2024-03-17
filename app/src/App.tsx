e750f17fbf42b32388a73fcb3182f5d8689d9526
Anna Borzenko <annnechko@gmail.com>
2024-03-17 22:23:58 +1300

    add field view

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,29 +5,52 @@ import styles from './App.module.css';
 import Typography from '@mui/material/Typography';
 import {FieldConfiguration} from "./configuration/FieldConfiguration";
 
+enum FieldType {
+  input = "input",
+  textarea = "textarea",
+}
+
+type FieldViewProps = {
+  type: FieldType
+};
+
+function FieldView(options: FieldViewProps) {
+  if (options.type === FieldType.input) {
+    return <>
+      input
+    </>
+  } else if (options.type === FieldType.textarea) {
+    return <>
+      textarea
+    </>
+  }
+
+}
+
 export default function App() {
 
   return (
     <div className={styles.content}>
       <Card className={styles.card}>
         <CardContent>
-
-         <FieldConfiguration/>
+          <Typography variant="h4" component="div">
+            Configure
+          </Typography>
+          <Typography variant="body1" component="div" sx={{mt: 2}}>
+          <FieldConfiguration/>
+        </Typography>
         </CardContent>
 
       </Card>
       <Card className={styles.card}>
         <CardContent>
-
           <Typography variant="h4" component="div">
             Preview
           </Typography>
-
-          <Typography variant="body1">
-            well meaning and kindly.
-            <br/>
-            {'"a benevolent smile"'}
+          <Typography variant="body1" sx={{mt: 2}}>
+            <FieldView type={FieldType.textarea}/>
           </Typography>
+
         </CardContent>
 
       </Card>
--- app/src/configuration/FieldConfiguration.tsx
+++ app/src/configuration/FieldConfiguration.tsx
@@ -12,26 +12,21 @@ export function FieldConfiguration() {
     setType(event.target.value);
   };
   return <>
-    <Typography variant="h4" component="div">
-      Configure
-    </Typography>
-    <Typography variant="body1" component="div">
-      <TextField id="outlined-basic" label="Field name" variant="outlined" size="small" fullWidth sx={{mt: 2}}/>
-      <FormControl sx={{mt: 2, minWidth: 120}} fullWidth>
+
+      <TextField id="outlined-basic" label="Field name" variant="outlined" size="small" fullWidth />
+      <FormControl sx={{mt: 2, minWidth: 120}} fullWidth size="small">
         <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
         <Select
           labelId="demo-simple-select-helper-label"
           id="demo-simple-select-helper"
-
           value={type}
           label="Type"
           onChange={handleTypeChange}
         >
-
           <MenuItem value={'input'}>input</MenuItem>
           <MenuItem value={'textarea'}>textarea</MenuItem>
         </Select>
       </FormControl>
-    </Typography>
+
   </>
 }
