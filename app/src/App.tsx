e9f0333aeea798ad1224c209613b198eb0fb519f
Anna Borzenko <annnechko@gmail.com>
2024-03-17 19:47:56 +1300

    separate FieldConfiguration

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -9,37 +9,44 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, {SelectChangeEvent} from '@mui/material/Select';
 
-export default function App() {
-  const [age, setAge] = React.useState('input');
-  const handleChange = (event: SelectChangeEvent) => {
-    setAge(event.target.value);
+function FieldConfiguration() {
+  const [type, setType] = React.useState('input');
+  const handleTypeChange = (event: SelectChangeEvent) => {
+    setType(event.target.value);
   };
+  return <>
+    <Typography variant="h4" component="div">
+      Configure
+    </Typography>
+    <Typography variant="body1" component="div">
+      <TextField id="outlined-basic" label="Field name" variant="outlined" size="small" fullWidth sx={{mt: 2}}/>
+      <FormControl sx={{mt: 2, minWidth: 120}} fullWidth>
+        <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+
+          value={type}
+          label="Type"
+          onChange={handleTypeChange}
+        >
+
+          <MenuItem value={'input'}>input</MenuItem>
+          <MenuItem value={'textarea'}>textarea</MenuItem>
+        </Select>
+      </FormControl>
+    </Typography>
+  </>
+}
+
+export default function App() {
+
   return (
     <div className={styles.content}>
       <Card className={styles.card}>
         <CardContent>
 
-          <Typography variant="h4" component="div">
-            Configure
-          </Typography>
-          <Typography variant="body1" component="div">
-            <TextField id="outlined-basic" label="Field name" variant="outlined" size="small" fullWidth sx={{mt: 2}}/>
-            <FormControl sx={{mt: 2, minWidth: 120}} fullWidth>
-              <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-
-                value={age}
-                label="Type"
-                onChange={handleChange}
-              >
-
-                <MenuItem value={'input'}>input</MenuItem>
-                <MenuItem value={'textarea'}>textarea</MenuItem>
-              </Select>
-            </FormControl>
-          </Typography>
+         <FieldConfiguration/>
         </CardContent>
 
       </Card>
