c6e4e10d863020500b0e918f91af9669fb6e169a
Anna Borzenko <annnechko@gmail.com>
2024-03-17 19:44:05 +1300

    add mui,2 cards with form name and type

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css' ;
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import styles from './App.module.css';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
 
-function App() {
+export default function App() {
+  const [age, setAge] = React.useState('input');
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
   return (
     <div className={styles.content}>
-      <header className={styles.header}>
-        <img src={logo} className={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card className={styles.card}>
+        <CardContent>
+
+          <Typography variant="h4" component="div">
+            Configure
+          </Typography>
+          <Typography variant="body1" component="div">
+            <TextField id="outlined-basic" label="Field name" variant="outlined" size="small" fullWidth sx={{mt: 2}}/>
+            <FormControl sx={{mt: 2, minWidth: 120}} fullWidth>
+              <InputLabel id="demo-simple-select-helper-label">Type</InputLabel>
+              <Select
+                labelId="demo-simple-select-helper-label"
+                id="demo-simple-select-helper"
+
+                value={age}
+                label="Type"
+                onChange={handleChange}
+              >
+
+                <MenuItem value={'input'}>input</MenuItem>
+                <MenuItem value={'textarea'}>textarea</MenuItem>
+              </Select>
+            </FormControl>
+          </Typography>
+        </CardContent>
+
+      </Card>
+      <Card className={styles.card}>
+        <CardContent>
+
+          <Typography variant="h4" component="div">
+            Preview
+          </Typography>
+
+          <Typography variant="body1">
+            well meaning and kindly.
+            <br/>
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+
+      </Card>
     </div>
   );
 }
-
-export default App;
